ce37d9e323e45427d8a20f691163c500d6a83153
Kiptanui Boaz <84452739+kiptanuiBoaz@users.noreply.github.com>
2022-05-27 04:29:10 +0300

    Refactor

--- src/index.js
+++ src/index.js
@@ -6,29 +6,35 @@ ReactDOM.render(<App />, document.getElementById("root"));
 
 var numbers = [3, 56, 2, 48, 5];
 
-const newNumbers= numbers.map((x)=>{return x*x})
- console.log(newNumbers);
-////Map -Create a new array by doing something with each item in an array.
-// const newNumbers = numbers.map(function (x) {
-//   return x * 2;
-// });
+const newNumbers = numbers.map((x) => {
+  return x * x;
+});
+console.log(newNumbers);
 
-//////Filter - Create a new array by keeping the items that return true.
-// const newNumbers = numbers.filter(function(num) {
-//   return num < 10;
-// });
+//Map -Create a new array by doing something with each item in an array.
+const newNumbers2 = numbers.map((x) => x * 2);
+console.log(newNumbers2);
 
-//Reduce - Accumulate a value by doing something to each item in an array.
-// var newNumber = numbers.reduce(function (accumulator, currentNumber) {
-//     return accumulator + currentNumber;
-// })
+////Filter - Create a new array by keeping the items that return true.
+const newNumbers3 = numbers.filter((num) => {
+  return num < 10;
+});
+console.log(newNumbers3);
 
-////Find - find the first item that matches from an array.
-// const newNumber = numbers.find(function (num) {
-//   return num > 10;
-// })
+// Reduce - Accumulate a value by doing something to each item in an array.
+var newNumbers4 = numbers.reduce(
+  (accumulator, currentNumber) => accumulator + currentNumber
+);
+console.log(newNumbers4);
 
-////FindIndex - find the index of the first item that matches.
-// const newNumber = numbers.findIndex(function (num) {
-//   return num > 10;
-// })
+//Find - find the first item that matches from an array.
+const newNumbers5 = numbers.find((num) => {
+  return num > 10;
+});
+console.log(newNumbers5);
+
+//FindIndex - find the index of the first item that matches.
+const newNumbers6 = numbers.findIndex((num) => {
+  return num > 10;
+});
+console.log(newNumbers6);
